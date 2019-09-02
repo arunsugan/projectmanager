86c8f50b4d848fa3c0dd1618c9177e76452f7db5
arun <arunsugan08@gmail.com>
2019-09-01 23:59:44 -0400

    initial commit for task comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskCreateComponent
   ],
   imports: [
     BrowserModule,
