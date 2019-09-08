3b93badf37a7a1914a8b0e1b2b86c431ceff96fa
arun <arunsugan08@gmail.com>
2019-09-08 16:17:33 -0400

    inital - Created all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskCreateComponent } from './task-create/task-create.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { TaskViewComponent } from './task-view/task-view.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { ProjectCreateComponent } from './project-create/project-create.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectViewComponent } from './project-view/project-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskCreateComponent
+    TaskCreateComponent,
+    UserCreateComponent,
+    UserViewComponent,
+    UserDetailComponent,
+    TaskViewComponent,
+    TaskDetailComponent,
+    ProjectCreateComponent,
+    ProjectDetailComponent,
+    ProjectViewComponent
   ],
   imports: [
     BrowserModule,
