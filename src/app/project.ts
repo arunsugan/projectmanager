935abd91d88a040c5599e74c7813607d7e1eda15
arun <arunsugan08@gmail.com>
2019-09-08 17:49:03 -0400

    Added initial interface project task user

---
+++ src/app/project.ts
@@ -0,0 +1,7 @@
+export class project {
+    project: number;
+    startEndDate: boolean;
+    startDate: string;
+    endDate: string;
+    manager: string;
+}
---
+++ src/app/task.ts
@@ -0,0 +1,10 @@
+export class task {
+    id: number;
+    project: string;
+    task: string;
+    priority: number;
+    parentTask: number;
+    startDate: string;
+    endDate: string;
+    user: string;
+}
