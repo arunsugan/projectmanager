08a2726f0648a615eed73a450de4029744718d01
arun <arunsugan08@gmail.com>
2019-09-29 12:12:04 -0400

    Moved components to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskCreateComponent } from './task-create/task-create.component';
-import { UserCreateComponent } from './user-create/user-create.component';
-import { UserViewComponent } from './user-view/user-view.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { TaskViewComponent } from './task-view/task-view.component';
-import { TaskDetailComponent } from './task-detail/task-detail.component';
-import { ProjectCreateComponent } from './project-create/project-create.component';
-import { ProjectDetailComponent } from './project-detail/project-detail.component';
-import { ProjectViewComponent } from './project-view/project-view.component';
+import { TaskCreateComponent } from './components/task-create/task-create.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { TaskViewComponent } from './components/task-view/task-view.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { ProjectCreateComponent } from './components/project-create/project-create.component';
+import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
+import { ProjectViewComponent } from './components/project-view/project-view.component';
 
 @NgModule({
   declarations: [
